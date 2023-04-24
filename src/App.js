d021670e09ef852a232362fd0b831ff23e7dc2bf
Diamachi <peeyushyadav1402@gmail.com>
2023-04-24 13:13:58 +0000

    testing-3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       {/* {datas.map(data => (
         <CardComponent title={data.title} company={data.company} location={data.location} type={data.type} exp={data.experience} stipend={data.stipend} empNo={data.employees} logo={data.logo} />
       ))} */}
-      <h1>Waddup My GG</h1>
+      <h1>Waddup My Brothers</h1>
       )
     </div>
   );
