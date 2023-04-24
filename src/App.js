7b627fbfe8acae7be0a2fdbd88999fbede1cbe74
Diamachi <peeyushyadav1402@gmail.com>
2023-04-24 13:07:47 +0000

    testing

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { datas } from './data'
 function App() {
   return (
     <div className="App">
-      {datas.map(data => (
+      {/* {datas.map(data => (
         <CardComponent title={data.title} company={data.company} location={data.location} type={data.type} exp={data.experience} stipend={data.stipend} empNo={data.employees} logo={data.logo} />
-      ))}
+      ))} */}
+      <h1>Waddup My GG</h1>
       )
     </div>
   );
